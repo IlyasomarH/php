027196ef6a4ad84487fa1473607a4df72e14b39d
ilyas <ilyasomar8857@gmail.com>
2022-07-28 20:35:39 +0300

    variable

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php
+
+// $nom="ILYAS OMAR HOUSSEIN ";
+// $formation="Programmation web";
+// $note_HTML5_CSS3=0;
+// $note_git=18;
+// $note_figma=11;
+// $note_js=17;
+// $note_botstrap=10;
+// $note_php=1; 
+// $moyenne_Generale=0;
+$Admin=false;
+$nombreMatier=6;
+
+$eleve1=[
+    'nom'=> 'ILYAS OMAR HOUSSEIN',
+    'formation'=> 'Prgrammation',
+    'note_HTML5_CSS3'=> 0,
+    'note_git'=>0,
+    'note_figma'=>12,
+    'note_js'=>10,
+    'note_botstrap'=>18,
+    'note_php'=>18
+];
+$eleve2=[
+    'nom'=> 'ALi OMAR HOUSSEIN',
+    'formation'=> 'Prgrammation',
+    'note_HTML5_CSS3'=> 10,
+    'note_git'=>19,
+    'note_figma'=>12,
+    'note_js'=>20,
+    'note_botstrap'=>18,
+    'note_php'=>18
+];
+
+
+
+
+/*     $taille[0]=120, $taille[1]=130, $taille[2]=160, $taille[3]=190
+
+
+*/
+
+echo $eleve2['nom'];
+
+// premier chose 
+/*
+
+
+tout d'abort il faut que je calcule note moyenne Generale 
+moyenG= somme 
+
+*/
+// $moyenne_Generale= ($note_HTML5_CSS3 + $note_git + $note_figma + $note_js + $note_botstrap + $note_php)/$nombreMatier;
+
+
+// for($i=1; $i<=10; $i++){
+//     echo "bonsoir \n";
+// }
+
+$i=false;
+// while($i==true){
+//     echo "bonsoir \n";
+    
+// }
+
+// $j=1;
+// do{
+//     echo "bonsoir \n";
+  
+// }while($i==true)
+
+
+// if($moyenne_Generale >10){
+//     if($moyenne_Generale>=10 && $moyenne_Generale<12){
+//         echo "M. $nom vous avez eu avec mention passable ";
+//     }else if($moyenne_Generale>=12 && $moyenne_Generale<14){
+//         echo "M. $nom vous avez eu avec mention assez bien ";
+//     }else if($moyenne_Generale>=14 && $moyenne_Generale<15){
+//         echo "M. $nom vous avez eu avec mention  bien ";
+//     }elseif($moyenne_Generale>=15){
+//         echo "Felicitation M. $nom vous avez eu avec mention  Trés-bien ";
+//     }
+
+// }else{
+//     echo "M. $nom vous etes ajourné ";
+// }
+// echo "M. $nom a eu  $moyenne_Generale  au moyenne Generale \n" ;
+
+
+
+
+
+
+// echo "Bonsoir M. $nom son taille est $taille cm \n  ";
+
+// echo 'Bonsoir M. '. $nom . ' son taille est ' . $taille . ' cm';
+
+
+// if($nom=='ILYAS' && $taille==170){
+
+//     echo 'Bonsoir M. '.$nom;
+
+// }else{
+//     echo "n'etes pas autorise a renté";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
